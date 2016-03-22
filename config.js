103fbcc25df9ec19ae00bffee3fc79311ae849e2
Branden Dane <branden@danetheory.com>
2016-03-22 00:50:59 -0400

    make it better

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  twitter: {
+    consumer_key: 'iwA5ppC0YDUzVMTNmIoQcxZWN',
+    consumer_secret: '72UEQD97R8LpQUlMZAIAIydCzdUweFe8hSqqfuNLkNtiVRlHn6',
+    access_token_key: '338583135-m22QNxiUS8dVsZKgYSZc4iU54FY7EkdmUhVugEDm',
+    access_token_secret: 'ElYdVBjPt4arkJRQSi3XcegSK0LfNwU2zvVTs8rvJy2K1'
+  }
+}
